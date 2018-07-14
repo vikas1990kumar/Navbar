a93cbb6cca937b01034d4ccd675e13696d2f2634
vikas1990kumar <kumar1990vikas@gmail.com>
2018-07-15 01:43:21 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+     
+
+     $("document").ready(() => {
+      
+      /*$("#tog").hide();*/
+      
+      $(".container .row1").hide();
+
+      $("#tog").click(() =>{
+      
+        $(".container .row1").slideToggle();
+      })
+
+
+     })
+
